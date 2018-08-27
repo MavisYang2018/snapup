eac3f65df8aea910ec4d2484950881ded2817627
penny.yang <penny.yang@cvilux.com>
2018-08-27 10:17:01 +0800

    structrue adjust

--- web-app/js/camera/template.js
+++ web-app/js/camera/template.js
@@ -4,10 +4,12 @@ cameraTemplate.template = $(".main").html(
     <div class="camera-layout">
       <div lcass="camera-content">
         <video id="camera-video" class="mobile-col-camera-video" autoplay></video>
-        <button id="camera-snap" class="mobile-col-camera-btn-snap noselect">Snap Photo</button>
         <canvas id="camera-canvas" class="mobile-col-camera-canvas"></canvas>
-        <button id="camera-upload" class="mobile-col-camera-btn-upload noselect">Upload</button>
-      </div>
+        <div id="btn" class="btn-box">
+          <button id="camera-snap" class="mobile-col-camera-btn-snap noselect">Snap Photo</button>
+          <button id="camera-upload" class="mobile-col-camera-btn-upload noselect">Upload</button>
+          </div>
+        </div>
     </div>
     `
 );
